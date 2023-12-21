9250b04d723331e5056dd413c85ba42796370b3b
Connor Rhodes <connor@rhodes.contact>
2023-12-21 10:47:24 -0600

    added music_library_adjust script

---
+++ music_library_adjust.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Script for transcoding music files and moving them to a server directory.
+
+# CONFIG
+music_archive = '/zstore/newmedia/music/music_archive/'
+music_serve = '/zstore/newmedia/music/music_serve/'
+vol_adjust = '0.03'
+
+import os
+
+# copy contents of music_archive to music_serve overwriting existing files
+os.system(f'cp -r {music_archive}/* {music_serve}/')
+
+for root, dirs, files in os.walk(plex_music_dir):
+    for file in files:
+        if file.rsplit('.', maxsplit=1)[-1] in ['mp3', 'm4a']:
+            filepath = os.path.join(root, file)
+            temp_file_path = os.path.join(root, ('q_' + file))
+
+            os.system(
+                    f'ffmpeg -i "{filepath}" -vn -filter:a "volume={vol_adjust}" "{temp_file_path}"'
+            )
+
+            os.remove(filepath)
+
+            os.rename(temp_file_path, filepath)
+
