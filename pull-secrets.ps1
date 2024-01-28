6020ee8b7026ebc43f94d385b0e7c329680b3874
Connor Rhodes <connor@rhodes.contact>
2024-01-28 14:42:30 -0600

    added TODOs to script

--- pull-secrets.ps1
+++ pull-secrets.ps1
@@ -1,10 +1,31 @@
 #!/usr/bin/env pwsh
 # Script to pull secrets used in the given project from a custom static file server vault
 
-param (
+function Get-Secrets {
+    param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Url,
     [Parameter(Mandatory = $true)]
     [string]
     $Token
-)
+    )
 
-Write-Host $Token
+    # TODO: store current directory in $Folder
+    # TODO: parse .gitignore and for-each file in gitignore, try invoke-webrequest ignorring 404s 
+
+    # main request
+    # Invoke-WebRequest -Uri ($Url + $Folder + $File + '?' + $Token) -OutFile $File
+}
+
+if (Test-Path -Path ".vaultsecrets.json") {
+    $secrets = Get-Content -Path ".vaultsecrets.json" | ConvertFrom-Json
+    $Url = $secrets.Url
+    $Token = $secrets.Token
+
+    Get-Secrets -Url $Url -Token $Token
+}
+else {
+    Get-Secrets
+    # TODO: offer to save $Url and $Token in .vaultsecrets.json
+}
