d289eb1b5049ec8712124bc3a287dbe36b40f650
Connor Rhodes <connor@rhodes.contact>
2024-02-04 11:19:06 -0600

    added adjustAbookVolume script

---
+++ adjustAbookVolume.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh
+# Script to adjust the volume of m4b audiobook files in subdirectories of the current directory
+
+Get-ChildItem -Directory | ForEach-Object {
+    $directory = $_.FullName
+    $m4bFiles = Get-ChildItem $directory -Filter "*.m4b" | Select-Object -ExpandProperty FullName
+
+    foreach ($file in $m4bFiles) {
+        $fileName = Split-Path -Path $file -Leaf
+        $directoryPath = Split-Path -Path $file -Parent
+    
+        ffmpeg -i "$file" -vn -filter:a 'volume=0.07' ($directoryPath + '/07' + $fileName)
+
+        Rename-Item -Path $file -NewName ($file + '.original')
+    }
+}
