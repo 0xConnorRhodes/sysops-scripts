208d7d61b8b346daf03c409e4983833c33f8a22f
Connor Rhodes <connor@rhodes.contact>
2024-03-24 09:30:28 -0500

    updated podcasts_convert

--- podcast_convert.ps1
+++ podcast_convert.ps1
@@ -4,9 +4,8 @@
 # CONFIG
 $silent_speed = '20'
 $vol_adjust = '0.07'
-#$input_dir = '/zstore/media/podcasts/ashelf_podcasts'
-$inputDir = '/zstore/media/podcasts/testdir'
-$outputDir = '/zstore/media/audiobooks/audiobookshelf/podcasts'
+$inputDir = '/zstore/media/podcasts/podgrab'
+$outputDir = '/zstore/media/podcasts/ashelf_podcasts'
 
 $existingFiles = gci -Path $outputDir -File -Filter *.mp3 -Recurse
 $inputFiles = gci -Path $inputDir -File -Filter *.mp3 -Recurse | 
@@ -16,11 +15,15 @@ $inputFiles = gci -Path $inputDir -File -Filter *.mp3 -Recurse |
 
 
 foreach ($file in $inputFiles) {
+    # CALCULATE ARGS
     $filenameAltered = $file.Name -replace '\.mp3$', '_ALTERED.mp3'
-    auto-editor --no-open -s $silent_speed $file
     $filenameFinal = $file.Name -replace '\.mp3$', '_FINAL.mp3'
+    $finalFilePath = "$($file.DirectoryName)/$filenameFinal"
+    $finalFileDestination = "$outputDir/$($file.Directory.Name)/$($file.Name)"
+
+    # RUN COMMANDS
+    auto-editor --no-open -s $silent_speed $file
     ffmpeg -i $file -vn -filter:a "volume=$vol_adjust" "$($file.DirectoryName)/$filenameFinal"
-    $outputSubdir = $file.DirectoryName -replace '.*?_original ', ''
-    Move-Item -Path "$($file.DirectoryName)/$filenameFinal" -Destination "$outputDir/$outputSubdir/$($file.Name)"
+    Move-Item -Path $finalFilePath -Destination $finalFileDestination
     Remove-Item -Path "$($file.DirectoryName)/$filenameAltered"
 }
