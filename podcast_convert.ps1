1945624d72bdd6d6cba2cd7cd78b0905538ba8b2
Connor Rhodes <connor@rhodes.contact>
2024-03-28 14:01:31 -0500

    updated podcast_convert to use a 2 directory structure

--- podcast_convert.ps1
+++ podcast_convert.ps1
@@ -4,11 +4,14 @@
 # CONFIG
 $silent_speed = '20'
 $vol_adjust = '0.07'
-$podcastsDir = '/zstore/media/podcasts/ashelf_podcasts'
+$inputDir = '/zstore/media/podcasts/ashelf_podcasts/podcast_originals'
+$outputDir = '/zstore/media/podcasts/ashelf_podcasts/podcast_rss'
 
-$newFiles = gci -Path $podcastsDir -File -Filter *.mp3 -Recurse |
+$existingFiles = gci -Path $outputDir -File -Filter *.mp3 -Recurse
+$newFiles = gci -Path $inputDir -File -Filter *.mp3 -Recurse |
     ? Name -NotLike "*_FINAL.mp3" |
-    ? Name -NotLike "*_ALTERED.mp3"
+    ? Name -NotLike "*_ALTERED.mp3" |
+    ? Name -NotIn $existingFiles.Name
 
 foreach ($file in $newFiles) {
     $filenameAltered = $file.Name -replace '\.mp3$', '_ALTERED.mp3'
@@ -17,6 +20,5 @@ foreach ($file in $newFiles) {
     auto-editor --no-open -s $silent_speed $file
     ffmpeg -i $file -vn -filter:a "volume=$vol_adjust" "$($file.DirectoryName)/$filenameFinal"
     Remove-Item -Path "$($file.DirectoryName)/$filenameAltered"
-    Remove-Item -Path $file
-    Rename-Item -Path "$($file.DirectoryName)/$filenameFinal" -NewName $file
+    Move-Item -Path "$($file.DirectoryName)/$filenameFinal" -Destination "$outputDir/$($file.Directory.Name)/$($file.Name)"
 }
