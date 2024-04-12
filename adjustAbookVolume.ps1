c564fc4a91955ab5d0d50e97a174547aca393ec1
Connor Rhodes <connor@rhodes.contact>
2024-04-12 07:49:44 -0500

    updated adjustAbookVolume to move final file to output folder

--- adjustAbookVolume.ps1
+++ adjustAbookVolume.ps1
@@ -1,6 +1,9 @@
 #!/usr/bin/env pwsh
 # Script to adjust the volume of m4b audiobook files in subdirectories of the current directory
 
+$volAdjust = '0.08'
+$outputFolder = '/zstore/media/audiobooks/audiobookshelf/audiobooks'
+
 Get-ChildItem -Directory | ForEach-Object {
     $directory = $_.FullName
     $m4bFiles = Get-ChildItem $directory -Filter "*.m4b" | Select-Object -ExpandProperty FullName
@@ -10,9 +13,10 @@ Get-ChildItem -Directory | ForEach-Object {
         $directoryPath = Split-Path -Path $file -Parent
 	$tempName = $directoryPath + '/q' + $fileName
     
-        ffmpeg -i "$file" -vn -filter:a 'volume=0.08' $tempName
+        ffmpeg -i "$file" -vn -filter:a ('volume=' + $volAdjust) $tempName
 
-	Remove-Item -Path $file
+	Remove-Item -Path $file   
 	Rename-Item -Path $tempName -NewName $file
+        Move-Item -Path $directoryPath -Destination $outputFolder
     }
 }
