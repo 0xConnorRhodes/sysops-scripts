d58b3c11c9f93124d084b29e590a8e2794c1bd4f
Connor Rhodes <connor@rhodes.contact>
2024-04-15 08:25:26 -0500

    updated notes_sync script

--- note-automation/notes_sync.sh
+++ note-automation/notes_sync.sh
@@ -4,13 +4,14 @@ source .env # provides NOTES_PATH
 
 cd "$NOTES_PATH"
 
+git pull
+
 CHANGES_EXIST="$(git status --porcelain | wc -l)"
 
 if [ "$CHANGES_EXIST" -eq 0 ] ; then
 	exit 0
 fi
 
-git pull
 git add .
 git commit -q -m "autosync"
 git push -q
