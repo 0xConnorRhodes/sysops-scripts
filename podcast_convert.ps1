2cfd725e79679cf7aa9f8dfcfa5337d593034249
Connor Rhodes <connor@rhodes.contact>
2024-05-09 23:25:31 -0500

    updated podcast convert to run on pi with auto-editor in docker

--- podcast_convert.ps1
+++ podcast_convert.ps1
@@ -3,9 +3,9 @@
 
 # CONFIG
 $silent_speed = '20'
-$vol_adjust = '0.07'
-$inputDir = '/zstore/media/podcasts/ashelf_podcasts/podcast_originals'
-$outputDir = '/zstore/media/podcasts/ashelf_podcasts/podcast_rss'
+$vol_adjust = '0.08'
+$inputDir = '/media/podcasts/ashelf_casts/podcast_originals'
+$outputDir = '/media/podcasts/ashelf_casts/podcast_audioadjust'
 
 $existingFiles = gci -Path $outputDir -File -Filter *.mp3 -Recurse
 $newFiles = gci -Path $inputDir -File -Filter *.mp3 -Recurse |
@@ -17,7 +17,7 @@ foreach ($file in $newFiles) {
     $filenameAltered = $file.Name -replace '\.mp3$', '_ALTERED.mp3'
     $filenameFinal = $file.Name -replace '\.mp3$', '_FINAL.mp3'
 
-    auto-editor --no-open -s $silent_speed $file
+    docker exec -it auto-editor auto-editor --no-open -s $silent_speed $file
     ffmpeg -i $file -vn -filter:a "volume=$vol_adjust" "$($file.DirectoryName)/$filenameFinal"
     Remove-Item -Path "$($file.DirectoryName)/$filenameAltered"
     Move-Item -Path "$($file.DirectoryName)/$filenameFinal" -Destination "$outputDir/$($file.Directory.Name)/$($file.Name)"
